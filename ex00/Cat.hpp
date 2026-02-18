cc220581538844e4bb3598aefe8985fdf0895d13
LevlaQ <yildizghdersler@gmail.com>
2026-02-18 20:22:15 +0300

    Norm faults are fixed

--- ex00/Cat.hpp
+++ ex00/Cat.hpp
@@ -6,20 +6,13 @@
 /*   By: gyildiz <gyildiz@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 20:43:56 by gyildiz           #+#    #+#             */
-/*   Updated: 2026/02/12 17:10:59 by gyildiz          ###   ########.fr       */
+/*   Updated: 2026/02/18 20:20:00 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAT_HPP
 # define CAT_HPP
 # include "Animal.hpp"
-
-/*
-	Cat constructor'ını çalıştırırken eğer input alan
-	base class'ın constructor'ını çağırmasını istiyorsan
-	: Base (parameter) olarak bir belirtme yapmak zorundasın
-	Ama bu etkinlikte gerek yok gibi
-*/
 
 class Cat: public Animal
 {
--- ex02/Cat.hpp
+++ ex02/Cat.hpp
@@ -6,7 +6,7 @@
 /*   By: gyildiz <gyildiz@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 20:43:56 by gyildiz           #+#    #+#             */
-/*   Updated: 2026/02/13 13:13:59 by gyildiz          ###   ########.fr       */
+/*   Updated: 2026/02/18 20:21:30 by gyildiz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 # define CAT_HPP
 # include "Animal.hpp"
 # include "Brain.hpp"
-
-/*
-	Cat constructor'ını çalıştırırken eğer input alan
-	base class'ın constructor'ını çağırmasını istiyorsan
-	: Base (parameter) olarak bir belirtme yapmak zorundasın
-	Ama bu etkinlikte gerek yok gibi
-*/
 
 class Cat: public Animal
 {
